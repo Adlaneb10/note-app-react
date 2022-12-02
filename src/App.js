934ee342afa429d41fff06373258d1e8709bbbc2
Adlaneb10 <c19367031@mytudublin.ie>
2022-12-02 22:03:29 +0000

    changed event handing to use onchange for input

--- src/App.js
+++ src/App.js
@@ -3,32 +3,50 @@ import './App.css';
 
 function App() {
   const [notes, setNotes] = useState([]);
-
+  const [text, setText] = useState("");
+  let errorMsg = "ERROR: Input entered does not exist! Try again."
   const addNote = () => {
-    let text = document.getElementById("input-add").value;
-  notes.push(text);
+    notes.push(text);
+    console.log(notes);
+  
   }
-
+  
   const delNote = () => {
-    let text = document.getElementById('input-add').value; 
     let arrayPos = notes.indexOf(text);
     console.log(arrayPos);
-  }
+    if(arrayPos !==-1){
+      delete notes[arrayPos];
+      const newList = notes.filter((item) => item.arrayPos !== arrayPos)
+      setNotes(newList);
+      document.getElementById("error-input").innerHTML="";
+
+    }
+    if(arrayPos === -1) {
+      document.getElementById("error-input").innerHTML=errorMsg;
+    }
+  } 
 
   return (
     <div className="App">
       <div className='interactive-holder'>
-        <input type="text" id="input-add" placeholder="Add a note"/>
-        <button id="btnAdd" onClick={addNote} >Add</button>
+        <input type="text" id="input-add" onChange={(e) => setText(e.target.value)} placeholder="Add a note"/>
+        <button id="btnAdd" onClick={addNote}  >Add</button>
+        <button id="btnDel" onClick={delNote} >Delete</button>
+        <button id="btnGreen" onClick={delNote} >Green Notes</button>
+
+      </div>
+      <div id="error-input">
+
       </div>
       <div className='note-holder'>
         <ul>
-          {notes.map((words) => 
-            <li >{words}</li>
+          {notes.map(words => 
+            <li>{words}</li>
           )}
         </ul>
       </div>
     </div>
+    
   );
 }
 
