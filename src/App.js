7608f3094991657c89bf67fcbe5e3a352ff46923
Adlaneb10 <c19367031@mytudublin.ie>
2022-12-02 18:23:01 +0000

    cleaned

--- src/App.js
+++ src/App.js
@@ -2,9 +2,14 @@ import React from 'react';
 import './App.css';
 
 function App() {
+  const clicker = () => {
+    console.log("you clicked me")
+  }
+
   return (
     <div className="App">
-
+      <input type="text" id="inputadd"></input>
+      <button id="btnAdd" onClick={clicker()}></button>
     </div>
   );
 }
